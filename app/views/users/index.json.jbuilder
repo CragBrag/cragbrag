adcc55ae66a8014fcaac3f2e8462499f245e6002
galia <galiat@gmail.com>
2013-09-21 10:35:47 -0400

    Added Grade to user

--- app/views/users/index.json.jbuilder
+++ app/views/users/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@users) do |user|
-  json.extract! user, :first_name, :last_name, :email
+  json.extract! user, :first_name, :last_name, :email, :grade
   json.url user_url(user, format: :json)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130921122643) do
+ActiveRecord::Schema.define(version: 20130921142159) do
 
   create_table "climbs", force: true do |t|
     t.integer  "user_id"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20130921122643) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "facebook_id"
+    t.string   "grade"
   end
 
 end
