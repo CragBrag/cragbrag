acd1c5ad875a4e7f939062e75455dfdbbb2e1f9c
galia <galiat@gmail.com>
2013-09-21 01:26:34 -0400

    initialze project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cragbrag::Application.config.secret_key_base = '4847dcf03c1d1e47aca5e00de0a3b936d1c778c43c91302e4ca1adce1a3937b3ab24fd96b59791e95b04193d929ede4cbea32c06a1397f9974ed4f2cd414e9df'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cragbrag::Application.config.session_store :cookie_store, key: '_cragbrag_session'
