2aa0678a0f6e76110c98394f933a5237278dafaa
Bob Breznak <bob.breznak@gmail.com>
2013-09-22 11:34:31 -0400

    More fixes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,11 +8,11 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.find_by(facebook_id: auth.uid) || User.create_from_facebook(auth)
     session[:user_id] = user.id
-    redirect_to new_climb_path, notice: "Signed in!"
+    redirect_to new_climb_path
   end
   
   def destroy
     session[:user_id] = nil
-    redirect_to logged_out_path, notice: "Signed out!"
+    redirect_to logged_out_path
   end
 end
