ebf323991257491ea4899f1c861bfe1f249472d0
Bob Breznak <bob.breznak@gmail.com>
2013-09-22 12:04:21 -0400

    Fix once more

--- db/migrate/20130922135128_change_score_to_int.rb
+++ db/migrate/20130922135128_change_score_to_int.rb
@@ -6,7 +6,7 @@ class ChangeScoreToInt < ActiveRecord::Migration
     remove_column :users, :avg_bouldering_score
     remove_column :users, :grade
 
-    add_column :users, :cul_bouldering_score, :decimal, precision: 7, scale: 2
+    add_column :users, :cul_top_rope_score, :decimal, precision: 7, scale: 2
     add_column :users, :cul_bouldering_score, :decimal, precision: 3, scale: 0
     add_column :users, :avg_top_rope_score, :decimal, precision: 4, scale: 2
     add_column :users, :avg_bouldering_score, :decimal, precision: 4, scale: 2
