a8bc4088f0c97f1b093eacb56490c0259f05a1e2
Bob Breznak <bob.breznak@gmail.com>
2013-09-22 11:52:11 -0400

    Add Postgres hack

--- db/migrate/20130922135128_change_score_to_int.rb
+++ db/migrate/20130922135128_change_score_to_int.rb
@@ -1,8 +1,8 @@
 class ChangeScoreToInt < ActiveRecord::Migration
   def up
     change_table :users do |t|
-      t.change :cul_top_rope_score, :integer
-      t.change :cul_bouldering_score, :integer
+      t.change :cul_top_rope_score, :integer, "USING CAST(cul_top_rope_score AS integer)", 1
+      t.change :cul_bouldering_score, :integer, "USING CAST(cul_bouldering_score AS integer)", 1
 
       t.change :cul_bouldering_score, :decimal, precision: 7, scale: 2
       t.change :cul_bouldering_score, :decimal, precision: 3, scale: 0
